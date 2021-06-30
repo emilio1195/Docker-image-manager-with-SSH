3a89725deacbe444c4b0fb301737162b6e0cdde9
Emilio Burbano <carlos.b@lakatuna.com>
2021-06-30 18:09:56 -0500

    cambio de port

--- data.py
+++ data.py
@@ -7,7 +7,7 @@ name_folder_compose = 'files_compose'
 name_folder_docker_remote = 'docker_install'
 
 HOST = '34.132.49.242' #'141.94.16.193'
-PORT = 22
+PORT = 25
 USER = 'emilio1195' #'maintainer'
 PASS = '#emilio1195' #'MRXCQp48RMAh'
 
